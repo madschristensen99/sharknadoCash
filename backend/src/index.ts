c810726d6fd585571fa771ae6a9fcf78b261ce96
JohnPochta <johnpochta1@gmail.com>
2025-05-31 15:29:29 +0200

    backend endpoint done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import { createContext, getConfig } from '@vlayer/sdk/config';
 import { createVlayerClient } from '@vlayer/sdk';
 import { spawn } from "child_process";
 
+const proverAbi = [{"inputs":[],"name":"FailedInnerCall","type":"error"},{"inputs":[],"name":"DATA_URL","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string[]","name":"parts","type":"string[]"}],"name":"concatStrings","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[{"components":[{"internalType":"string","name":"webProofJson","type":"string"}],"internalType":"struct WebProof","name":"webProof","type":"tuple"},{"internalType":"string","name":"secretKey","type":"string"},{"internalType":"string","name":"txId","type":"string"},{"internalType":"address","name":"evmRecipientAddress","type":"address"},{"internalType":"string","name":"xmrRecipientAddress","type":"string"}],"name":"main","outputs":[{"components":[{"components":[{"internalType":"bytes4","name":"verifierSelector","type":"bytes4"},{"internalType":"bytes32[8]","name":"seal","type":"bytes32[8]"},{"internalType":"enum ProofMode","name":"mode","type":"uint8"}],"internalType":"struct Seal","name":"seal","type":"tuple"},{"internalType":"bytes32","name":"callGuestId","type":"bytes32"},{"internalType":"uint256","name":"length","type":"uint256"},{"components":[{"internalType":"address","name":"proverContractAddress","type":"address"},{"internalType":"bytes4","name":"functionSelector","type":"bytes4"},{"internalType":"uint256","name":"settleChainId","type":"uint256"},{"internalType":"uint256","name":"settleBlockNumber","type":"uint256"},{"internalType":"bytes32","name":"settleBlockHash","type":"bytes32"}],"internalType":"struct CallAssumptions","name":"callAssumptions","type":"tuple"}],"internalType":"struct Proof","name":"","type":"tuple"},{"internalType":"address","name":"","type":"address"},{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"proof","outputs":[{"components":[{"components":[{"internalType":"bytes4","name":"verifierSelector","type":"bytes4"},{"internalType":"bytes32[8]","name":"seal","type":"bytes32[8]"},{"internalType":"enum ProofMode","name":"mode","type":"uint8"}],"internalType":"struct Seal","name":"seal","type":"tuple"},{"internalType":"bytes32","name":"callGuestId","type":"bytes32"},{"internalType":"uint256","name":"length","type":"uint256"},{"components":[{"internalType":"address","name":"proverContractAddress","type":"address"},{"internalType":"bytes4","name":"functionSelector","type":"bytes4"},{"internalType":"uint256","name":"settleChainId","type":"uint256"},{"internalType":"uint256","name":"settleBlockNumber","type":"uint256"},{"internalType":"bytes32","name":"settleBlockHash","type":"bytes32"}],"internalType":"struct CallAssumptions","name":"callAssumptions","type":"tuple"}],"internalType":"struct Proof","name":"","type":"tuple"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"uint256","name":"blockNo","type":"uint256"}],"name":"setBlock","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"chainId","type":"uint256"},{"internalType":"uint256","name":"blockNo","type":"uint256"}],"name":"setChain","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+
 function runProcess(
   cmd: string,
   args: string[],
@@ -33,9 +35,9 @@ function runProcess(
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', async (_req, res) => {
+app.get('/api/zkproof/getProof', async (_req, res) => {
 
-  const { txid, key, address } = _req.query;
+  const { txid, key, address, ethereumRecipientAddress } = _req.query;
 
   const URL_TO_PROVE = `https://newrepo-production-1571.up.railway.app/verify?txid=${txid}&key=${key}&address=${address}`;
 
@@ -63,10 +65,13 @@ app.get('/', async (_req, res) => {
 
   console.log('config', config);
 
-  const webProof = await generateWebProof();
+  // const webProof = await generateWebProof();
 
   const webProof2 = await fetch('https://web-proof-vercel.vercel.app/api/handler', {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({
       url: URL_TO_PROVE,
       notary: "https://test-notary.vlayer.xyz",
@@ -74,14 +79,44 @@ app.get('/', async (_req, res) => {
     }),
   });
 
-  console.log(webProof2);
+  const webProof = await webProof2.json();
+
+  const { presentation } = webProof;
+
+  const presentationJson = JSON.parse(presentation);
+
+  console.log(presentationJson);
 
   console.log("✅ Web proof generated");
 
-  console.log("Web proof:", webProof);
-
-  res.send('Hello from TypeScript server!');
+  const hash = await vlayer.prove({
+    address: '0xe5986c4fe91d4d50c5716df805d877e31548c357',
+    functionName: "main",
+    proverAbi: proverAbi as any,
+    args: [
+      {
+        webProofJson: String(presentation),
+      },
+      key,
+      txid,
+      ethereumRecipientAddress,
+      address,
+    ],
+    chainId: chain.id,
+    gasLimit: config.gasLimit,
+  });
   
+  console.log("Proving hash:", hash);
+  
+  const result = await vlayer.waitForProvingResult({ hash });
+  
+  console.log(result);
+  const [proof] = result;
+  console.log("✅ Proof generated");
+
+  return res.json({
+    proof,
+  });
 });
 
 app.listen(port, () => {
